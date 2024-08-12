b4f281cbb714aba77b3333c90ddc3e298c813d17
A1ishka <alinamakogon123456@gmail.com>
2024-08-13 00:17:48 +0300

    Player logic_Exhibit State init 9

---
+++ common-ui/src/main/java/by/ssrlab/common_ui/common/util/ExhibitState.kt
@@ -0,0 +1,8 @@
+package by.ssrlab.common_ui.common.util
+
+import by.ssrlab.data.data.common.RepositoryData
+
+data class ExhibitState(
+    val header: String = "",
+    val repositoryData: RepositoryData? = null,
+)
