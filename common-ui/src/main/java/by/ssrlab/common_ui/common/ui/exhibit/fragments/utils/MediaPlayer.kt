68c15e42f3580526dbf5ce28195dc346233885cf
A1ishka <alinamakogon123456@gmail.com>
2024-08-12 17:40:08 +0300

    Player logic_PlayerState impl init 7

--- common-ui/src/main/java/by/ssrlab/common_ui/common/ui/exhibit/fragments/utils/MediaPlayer.kt
+++ common-ui/src/main/java/by/ssrlab/common_ui/common/ui/exhibit/fragments/utils/MediaPlayer.kt
@@ -1,66 +1,44 @@
 package by.ssrlab.common_ui.common.ui.exhibit.fragments.utils
 
 import android.media.MediaPlayer
-import android.media.PlaybackParams
-import android.view.View
+import android.net.Uri
 import android.widget.Toast
 import androidx.core.net.toUri
+import by.ssrlab.common_ui.common.ui.base.BaseActivity
 import by.ssrlab.common_ui.common.ui.exhibit.ExhibitActivity
 import by.ssrlab.common_ui.databinding.FragmentExhibitBinding
-import kotlinx.coroutines.*
-import java.io.File
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 object MediaPlayer {
 
     private var mediaPlayer: MediaPlayer? = null
     private val seekBarFuns = SeekBarFunctions()
 
-    private var speed: Float? = null
-    private var playerStatus = ""
-
     private val job = Job()
     private val scope = CoroutineScope(Dispatchers.IO + job)
 
-    fun initializeMediaPlayer(
-        activity: ExhibitActivity,
+    fun initializeMediaPlayerWithString(
+        activity: BaseActivity,
         binding: FragmentExhibitBinding,
-        file: File,
+        url: String,
         onSuccess: () -> Unit
     ) {
-        pauseAudio(binding)
-        playerStatus = "play"
         mediaPlayer = MediaPlayer()
 
-        val uri = file.toUri()
-
-        binding.apply {
-            exhibitPlayRipple.visibility = View.VISIBLE
-        }
-
         try {
-            mediaPlayer!!.setDataSource(activity, uri)
-
-            if (speed != null) {
-                val playBackParams = PlaybackParams()
-                playBackParams.speed = speed!!
-                mediaPlayer!!.playbackParams = playBackParams
-            }
-            mediaPlayer!!.prepare()
-
-            mediaPlayer!!.setOnPreparedListener {
+            setDataSource(activity, url.toUri())
+            mediaPlayer?.setOnPreparedListener {
                 binding.apply {
-//                    exhibitDurationBar.max = mediaPlayer!!.duration
-//                    exhibitDurationBar.progress = 0
-                    exhibitEndTime.text =
-                        seekBarFuns.convertToTimerMode(mediaPlayer!!.duration)
+                    exhibitEndTime.text = seekBarFuns.convertToTimerMode(mediaPlayer!!.duration)
                     exhibitCurrentTime.text =
                         seekBarFuns.convertToTimerMode(mediaPlayer!!.currentPosition)
                 }
-
                 listenProgress(binding)
                 onSuccess()
             }
-
         } catch (e: Exception) {
             activity.runOnUiThread {
                 Toast.makeText(activity, e.message, Toast.LENGTH_SHORT).show()
@@ -68,92 +46,135 @@ object MediaPlayer {
         }
     }
 
-    fun playAudio(activity: ExhibitActivity, binding: FragmentExhibitBinding) {
-        scope.launch {
-            when (playerStatus) {
-                "pause" -> {
-                    mediaPlayer!!.pause()
-                    playerStatus = "play"
-                }
-
-                "play" -> {
-                    try {
-                        mediaPlayer!!.start()
-                        playerStatus = "pause"
-                        initProgressListener(activity, binding)
-                    } catch (e: Exception) {
-                        Toast.makeText(activity, e.message, Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-        }
-    }
-
-    fun pauseAudio(binding: FragmentExhibitBinding) {
-        if (playerStatus == "pause") {
-            mediaPlayer!!.pause()
-            mediaPlayer!!.stop()
-            mediaPlayer!!.reset()
-
-            playerStatus = "play"
-        }
-    }
-
-    fun changeAudioSpeed(speed: Float, activity: ExhibitActivity, binding: FragmentExhibitBinding) {
-        val playBackParams = PlaybackParams()
-        playBackParams.speed = speed
-        this.speed = speed
-        mediaPlayer?.playbackParams = playBackParams
-
-        if (playerStatus == "play") {
-            mediaPlayer!!.pause()
-            playerStatus = "play"
-        }
-    }
-
-    private suspend fun initProgressListener(
+    fun handlePlayerState(
+        state: PlayerStatus,
         activity: ExhibitActivity,
         binding: FragmentExhibitBinding
     ) {
-        while (playerStatus == "pause") {
-            scope.launch {
-                activity.runOnUiThread {
-                    binding.apply {
-                        if (mediaPlayer?.isPlaying == true) {
-                            exhibitCurrentTime.text =
-                                seekBarFuns.convertToTimerMode(mediaPlayer!!.currentPosition)
-                            exhibitProgress.progress = mediaPlayer!!.currentPosition
-                        }
-                    }
+        when (state) {
+            is PlayerStatus.Playing -> {
+                try {
+                    mediaPlayer?.start()
+                } catch (e: Exception) {
+                    Toast.makeText(activity, e.message, Toast.LENGTH_SHORT).show()
                 }
             }
 
-            delay(10)
-
-            binding.exhibitEndTime.apply {
-                if (progress == max) {
-                    playerStatus = "play"
-
-                    mediaPlayer?.seekTo(0)
-                    scope.launch {
-                        activity.runOnUiThread {
-                            binding.apply {
-//                                exhibitPlayIc.setImageResource(R.drawable.ic_play)
-                                exhibitEndTime.text = 0.toString()
+            is PlayerStatus.Paused -> {
+                mediaPlayer?.pause()
+                scope.launch {
+                    activity.runOnUiThread {
+                        binding.apply {
+                            if (mediaPlayer?.isPlaying == true) {
                                 exhibitCurrentTime.text =
                                     seekBarFuns.convertToTimerMode(mediaPlayer!!.currentPosition)
+                                exhibitProgress.progress = mediaPlayer!!.currentPosition
                             }
                         }
                     }
                 }
             }
+
+            is PlayerStatus.Seeking -> {
+            }
+
+            is PlayerStatus.Ended -> {
+                mediaPlayer?.seekTo(0)
+                scope.launch {
+                    activity.runOnUiThread {
+                        binding.apply {
+                            exhibitEndTime.text = "0:00"
+                            exhibitCurrentTime.text =
+                                seekBarFuns.convertToTimerMode(mediaPlayer!!.currentPosition)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun mediaPlayerIsNull(
+        audio: String,
+        binding: FragmentExhibitBinding,
+        exhibitActivity: ExhibitActivity
+    ) {
+        val fragmentSettingsManager = FragmentSettingsManager(
+            binding = binding,
+            exhibitActivity = exhibitActivity
+        )
+        fragmentSettingsManager.initMediaPlayerWithString(audio)
+    }
+
+//    fun isPlaying(
+//        audio: String,
+//        binding: FragmentExhibitBinding,
+//        exhibitActivity: ExhibitActivity
+//    ): Boolean {
+//
+//        Log.d("isPlaying", "isPlaying")
+//
+//        if (mediaPlayer == null) Log.d("isPlaying", "mediaPlayerIsNull") //mediaPlayerIsNull(audio, binding, exhibitActivity)
+//
+//        Log.d("isPlaying", "isPlaying")
+//        return mediaPlayer?.isPlaying ?: false
+//    }
+
+    fun isPlaying(): Boolean {
+        return mediaPlayer?.isPlaying ?: false
+    }
+
+    fun playAudio(
+        activity: ExhibitActivity,
+        binding: FragmentExhibitBinding,
+        playerStatus: PlayerStatus
+    ) {
+        scope.launch {
+            when (playerStatus) {
+                PlayerStatus.Paused -> {
+                    mediaPlayer?.pause()
+                }
+
+                PlayerStatus.Playing -> {
+                    try {
+                        mediaPlayer?.start()
+                        handlePlayerState(PlayerStatus.Playing, activity, binding)
+                    } catch (e: Exception) {
+                        Toast.makeText(activity, e.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                else -> {
+                    Toast.makeText(activity, "Not implemented yet", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    fun pauseAudio() {
+        mediaPlayer?.let {
+            if (it.isPlaying) {
+                it.pause()
+            }
+            it.stop()
+            it.reset()
+        }
+    }
+
+    private fun setDataSource(
+        activity: BaseActivity, uri: Uri
+    ) {
+        try {
+            mediaPlayer?.setDataSource(activity, uri)
+            mediaPlayer?.prepareAsync()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
     private fun listenProgress(binding: FragmentExhibitBinding) {
-        binding.exhibitDurationBar.setOnSeekBarChangeListener(
-            seekBarFuns.createSeekBarProgressListener {
-                mediaPlayer!!.seekTo(it)
+        binding.exhibitProgress.setOnSeekBarChangeListener(
+            seekBarFuns.createSeekBarProgressListener { progress ->
+                mediaPlayer?.seekTo(progress)
                 scope.launch {
                     binding.exhibitCurrentTime.text =
                         seekBarFuns.convertToTimerMode(mediaPlayer!!.currentPosition)
--- common-ui/src/main/java/by/ssrlab/common_ui/common/ui/exhibit/fragments/utils/MediaPlayerController.kt
+++
@@ -1,5 +0,0 @@
-package by.ssrlab.common_ui.common.ui.exhibit.fragments.utils
-
-interface MediaPlayerController {
-    fun initMediaPlayer()
-}
