d18a975c1b7dca77a931e60506c1da3ef76432b7
A1ishka <alinamakogon123456@gmail.com>
2024-08-22 14:14:14 +0300

    Resources: Prepare Persons for Resource init 4

--- ui/src/main/java/by/ssrlab/ui/states/PlayerState.kt
+++
@@ -1,8 +0,0 @@
-package by.ssrlab.ui.states
-
-data class PlayerState(
-    val playerVisible: Boolean = false,
-
-    val currentPosition: Int = 0,
-    val duration: Int = 0
-)
--- ui/src/main/java/by/ssrlab/ui/states/PlayerStatus.kt
+++
@@ -1,8 +0,0 @@
-package by.ssrlab.ui.states
-
-sealed class PlayerStatus {
-    data object Playing : PlayerStatus()
-    data object Paused : PlayerStatus()
-    data object Seeking : PlayerStatus()
-    data object Ended : PlayerStatus()
-}
