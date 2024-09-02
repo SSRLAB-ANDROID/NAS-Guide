42e42af8f279ec3c34e39393cfff85250d1f743d
A1ishka <alinamakogon123456@gmail.com>
2024-09-02 23:03:41 +0300

    OrgAdds_AchievementsFragment prep init 3

--- common-ui/src/main/java/by/ssrlab/common_ui/common/ui/exhibit/fragments/exhibit/child/AchievementsFragment.kt
+++ common-ui/src/main/java/by/ssrlab/common_ui/common/ui/exhibit/fragments/exhibit/child/AchievementsFragment.kt
@@ -1,12 +1,21 @@
 package by.ssrlab.common_ui.common.ui.exhibit.fragments.exhibit.child
 
+import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModel
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.ssrlab.common_ui.common.ui.base.BaseFragment
+import by.ssrlab.common_ui.common.ui.exhibit.fragments.utils.DividerItemDecoration
+import by.ssrlab.common_ui.common.ui.exhibit.fragments.utils.OrgsAdditionalsAdapter
+import by.ssrlab.common_ui.common.vm.OrgsAdditionalsSharedVM
+import by.ssrlab.common_ui.databinding.FragmentAchievementsBinding
+import by.ssrlab.data.data.settings.remote.OrganizationLocale
 import by.ssrlab.domain.models.ToolbarControlObject
 
-class AchievementsFragment: BaseFragment() {
+class AchievementsFragment : BaseFragment() {
+
+    private lateinit var binding: FragmentAchievementsBinding
 
     override val toolbarControlObject = ToolbarControlObject(
         isBack = false,
@@ -14,11 +23,29 @@ class AchievementsFragment: BaseFragment() {
         isSearch = true,
         isDates = false
     )
+    override val fragmentViewModel: OrgsAdditionalsSharedVM by viewModels({ requireParentFragment() })
 
-    override val fragmentViewModel: ViewModel
-        get() = TODO("Not yet implemented")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val achievements = fragmentViewModel.orgsList.value
+        initRecyclerView(achievements ?: emptyList())
+    }
+
+    private fun initRecyclerView(achievements: List<OrganizationLocale>) {
+
+        binding.achievementsRv.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = OrgsAdditionalsAdapter(achievements) {
+                navigateNext(itemDecorationCount)
+            }
+            addItemDecoration(DividerItemDecoration(context))
+        }
+    }
 
     override fun initBinding(container: ViewGroup?): View {
-        TODO("Not yet implemented")
+
+        binding = FragmentAchievementsBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 }
