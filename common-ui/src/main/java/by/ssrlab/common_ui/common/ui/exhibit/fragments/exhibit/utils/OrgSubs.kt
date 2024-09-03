8d9ba884bd079283a6e72a4bea93e2db9cf8ac38
A1ishka <alinamakogon123456@gmail.com>
2024-09-03 15:07:50 +0300

    OrgAdds_SubScreens Management init 4

---
+++ common-ui/src/main/java/by/ssrlab/common_ui/common/ui/exhibit/fragments/exhibit/utils/OrgSubs.kt
@@ -0,0 +1,9 @@
+package by.ssrlab.common_ui.common.ui.exhibit.fragments.exhibit.utils
+
+sealed class OrgSubs{
+    data object  Achievements : OrgSubs()
+    data object  Contacts : OrgSubs()
+    data object  ResearchAreas : OrgSubs()
+    data object  Laboratories : OrgSubs()
+    data object  Departments : OrgSubs()
+}
