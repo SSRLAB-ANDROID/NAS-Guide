9535639a7f52935b3723a14a80967f7efa47ad15
A1ishka <alinamakogon123456@gmail.com>
2024-10-03 13:53:55 +0300

    ExhibitSearch_Search for (Interesting) Places init 12

--- ui/src/main/java/by/ssrlab/ui/fragments/history/PlacesFragment.kt
+++ ui/src/main/java/by/ssrlab/ui/fragments/history/PlacesFragment.kt
@@ -1,8 +1,12 @@
 package by.ssrlab.ui.fragments.history
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.ImageButton
+import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -40,6 +44,7 @@ class PlacesFragment: BaseFragment() {
         activityVM.apply {
             setHeaderImg(by.ssrlab.common_ui.R.drawable.header_places)
             setButtonAction(ButtonAction.BackAction, ::onBackPressed)
+            setButtonAction(ButtonAction.SearchAction, ::initSearchBar)
         }
 
         binding.apply {
@@ -53,6 +58,12 @@ class PlacesFragment: BaseFragment() {
 
         initAdapter()
         observeOnDataChanged()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        hideSearchBar()
     }
 
     override fun observeOnDataChanged() {
@@ -107,5 +118,61 @@ class PlacesFragment: BaseFragment() {
 
     override fun navigateNext(repositoryData: RepositoryData) {
         (activity as MainActivity).moveToExhibit(repositoryData)
+    }
+
+
+    //Search
+    private var toolbarSearchView: SearchView? = null
+
+    private fun searchBarInstance(): SearchView {
+        if (toolbarSearchView == null) {
+            toolbarSearchView = requireActivity().findViewById(R.id.toolbar_search_view)
+        }
+        return toolbarSearchView!!
+    }
+
+    override fun filterData(query: String) {
+        fragmentViewModel.filterData(query)
+    }
+
+    private fun showSearchResults() {
+        fragmentViewModel.filteredDataList.value?.let { adapter.updateData(it) }
+    }
+
+    private fun initSearchBar() {
+        val toolbarSearchView = searchBarInstance()
+
+        toolbarSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    filterData(it)
+                    showSearchResults()
+                    return true
+                }
+                showSearchResults()
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let {
+                    filterData(it)
+                    showSearchResults()
+                }
+                return true
+            }
+        })
+        toolbarSearchView.visibility = View.VISIBLE
+        toolbarSearchView.isIconified = false
+        val searchButton: ImageButton = requireActivity().findViewById(R.id.toolbar_search)
+        searchButton.visibility = View.GONE
+
+        toolbarSearchView.requestFocus()
+        val inputManager = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.showSoftInput(toolbarSearchView.findFocus(), InputMethodManager.SHOW_IMPLICIT)
+    }
+
+    override fun hideSearchBar() {
+        val toolbarSearchView = searchBarInstance()
+        toolbarSearchView.visibility = View.GONE
     }
 }
