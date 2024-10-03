be68fac18e85d4c92ea628cefb7840c069f174a9
A1ishka <alinamakogon123456@gmail.com>
2024-10-03 16:34:06 +0300

    ExhibitFilter_Filter impl 1 init 1

--- ui/src/main/java/by/ssrlab/ui/vm/FOrgsVM.kt
+++ ui/src/main/java/by/ssrlab/ui/vm/FOrgsVM.kt
@@ -4,12 +4,16 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import by.ssrlab.common_ui.common.ui.base.vm.BaseFragmentVM
 import by.ssrlab.data.data.common.DescriptionData
+import by.ssrlab.data.data.remote.DepartmentFilter
 import by.ssrlab.data.data.settings.remote.OrganizationLocale
 import by.ssrlab.domain.repository.network.OrgsRepository
 import by.ssrlab.domain.utils.Resource
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-class FOrgsVM(orgsRepository: OrgsRepository): BaseFragmentVM<OrganizationLocale>(orgsRepository) {
+class FOrgsVM(orgsRepository: OrgsRepository) : BaseFragmentVM<OrganizationLocale>(orgsRepository) {
 
+    //Appearance
     private val _title = MutableLiveData("")
     val title: LiveData<String>
         get() = _title
@@ -18,7 +22,7 @@ class FOrgsVM(orgsRepository: OrgsRepository): BaseFragmentVM<OrganizationLocale
         _title.value = value
     }
 
-
+    //Data
     private val _orgsData = MutableLiveData<Resource<List<OrganizationLocale>>>()
     val orgsData: LiveData<Resource<List<OrganizationLocale>>> get() = _orgsData
 
@@ -53,7 +57,7 @@ class FOrgsVM(orgsRepository: OrgsRepository): BaseFragmentVM<OrganizationLocale
         loadData()
     }
 
-
+    //Filter and Search
     private val _filteredData = MutableLiveData<List<OrganizationLocale>>()
     val filteredDataList: LiveData<List<OrganizationLocale>> get() = _filteredData
 
@@ -61,6 +65,7 @@ class FOrgsVM(orgsRepository: OrgsRepository): BaseFragmentVM<OrganizationLocale
         _filteredData.value = value
     }
 
+    //Search
     fun filterData(query: String) {
         val resource = _orgsData.value
         if (resource is Resource.Success) {
@@ -69,5 +74,38 @@ class FOrgsVM(orgsRepository: OrgsRepository): BaseFragmentVM<OrganizationLocale
             }
             setFilteredList(filteredList)
         }
+    }
+
+    //Filter
+    private val _availableFilters = MutableStateFlow<Set<DepartmentFilter>>(emptySet())
+    val availableFilters: StateFlow<Set<DepartmentFilter>> = _availableFilters
+
+    private val _selectedFilters = MutableStateFlow<Set<DepartmentFilter>>(emptySet())
+    val selectedFilters: StateFlow<Set<DepartmentFilter>> = _selectedFilters
+
+    fun setAvailableFilters() {
+        val uniqueDepartmentFilters: Set<DepartmentFilter> =
+            if (_orgsData.value is Resource.Success) {
+                (_orgsData.value as Resource.Success<List<OrganizationLocale>>).data
+                    .map { it.description.departmentFilter }
+                    .toSet()
+            } else {
+                emptySet()
+            }
+
+        _availableFilters.value = uniqueDepartmentFilters
+    }
+
+    fun filterDataByChoices(){
+        val filterDataByChoices =
+            if (_orgsData.value is Resource.Success) {
+                val currentSelectedFilters = _selectedFilters.value
+                (_orgsData.value as Resource.Success<List<OrganizationLocale>>).data
+                    .filter { it.description.departmentFilter in currentSelectedFilters }
+            } else {
+                emptyList()
+            }
+
+        setFilteredList(filterDataByChoices)
     }
 }
