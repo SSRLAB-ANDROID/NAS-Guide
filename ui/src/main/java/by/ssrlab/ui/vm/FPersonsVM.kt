394300e83f3d9b16ca27f696d38a0dc637b0ebe1
A1ishka <alinamakogon123456@gmail.com>
2024-10-03 13:48:27 +0300

    ExhibitSearch_Search for Persons init 11

--- ui/src/main/java/by/ssrlab/ui/vm/FPersonsVM.kt
+++ ui/src/main/java/by/ssrlab/ui/vm/FPersonsVM.kt
@@ -3,12 +3,15 @@ package by.ssrlab.ui.vm
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import by.ssrlab.common_ui.common.ui.base.vm.BaseFragmentVM
+import by.ssrlab.data.data.settings.remote.OrganizationLocale
 import by.ssrlab.data.data.settings.remote.PersonLocale
 import by.ssrlab.domain.repository.network.PersonsRepository
+import by.ssrlab.domain.utils.Resource
 import by.ssrlab.ui.states.PersonsState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import java.util.Locale
 
 class FPersonsVM(personsRepository: PersonsRepository) :
     BaseFragmentVM<PersonLocale>(personsRepository) {
@@ -32,6 +35,24 @@ class FPersonsVM(personsRepository: PersonsRepository) :
                 currentState.copy(personList = personList.toMutableList())
             }
             _personsData.value = personState.value.personList?.toList()
+        }
+    }
+
+
+    private val _filteredData = MutableLiveData<List<PersonLocale>>()
+    val filteredDataList: LiveData<List<PersonLocale>> get() = _filteredData
+
+    private fun setFilteredList(value: List<PersonLocale>) {
+        _filteredData.value = value
+    }
+
+    fun filterData(query: String) {
+        val queryLowered = query.lowercase(Locale.getDefault())
+        _personsData.value?.let { data ->
+            val filteredList = data.filter {
+                it.name.contains(queryLowered, ignoreCase = true)
+            }
+            setFilteredList(filteredList)
         }
     }
 }
